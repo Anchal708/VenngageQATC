b6de575e1915d4d86287aa7bcd1cb0c2e25bf46c
Anchal708 <62824633+Anchal708@users.noreply.github.com>
2020-08-01 21:59:09 -0400

    Add files via upload

--- src/main/objects/Account.java
+++ src/main/objects/Account.java
@@ -1,14 +1,35 @@
 public class Account {
-	
-	/**
-	* Create the constructor that takes the following:
-	* First name
-	* Last name
-	* Email
-	* Password
-	**/
-	
-	
-	
-	// Create the necessary Getter functions
+	Account() {
+
+	}
+
+	String Firstname;
+	String Lastname;
+	String Email;
+	String Password;
+
+	Account(String Firstname, String Lastname, String Email, String Password) {
+		this.Firstname = Firstname;
+		this.Lastname = Lastname;
+		this.Email = Email;
+		this.Password = Password;
+
+	}
+
+	public String getFirstName() {
+		return Firstname;
+	}
+
+	public String getLastName() {
+		return Lastname;
+	}
+
+	public String Email() {
+		return Email;
+	}
+
+	public String Password() {
+		return Password;
+	}
+
 }
