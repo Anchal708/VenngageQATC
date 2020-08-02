f5c7c860ccf8a009cfcba345f0bba212a2a3ba22
Anchal708 <62824633+Anchal708@users.noreply.github.com>
2020-08-01 22:00:54 -0400

    Add files via upload

--- src/main/util/Selenium.java
+++ src/main/util/Selenium.java
@@ -1,38 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class Selenium {
-	
-	// Assume that the WebDriver manages browser actions.
+
 	private WebDriver driver;
-	
-	public Selenium (){
-		driver = new WebDriver();
+
+	public Selenium() {
+		driver = new ChromeDriver();
 	}
-	
-	/**
-	* Navigate to specified URL.
-	* @param url to navigate to.
-	**/
-	public void goToUrl(String url) {
-        driver.get(url);
-    }
-	
-	/**
-     * Simulates typing keys into the specified xpath
-     *
-     * @param xpath xpath of text field to send keys to
-     * @param keys  keys to send to xpath
-     */
-    public void sendKeys(String xpath, String keys) {
-        driver.findElement(By.xpath(xpath)).sendKeys(keys);
-    }
-	
-	/**
-     * Clicks the specified xpath.
-     * Assume that the click function simulates a left mouse click.
-     *
-     * @param xpath xpath to click
-     */
-    public void clickByXpath(String xpath) {
-        click(By.xpath(xpath));
-    }
-	
+
+	public void goToUrl() {
+		driver.get("https://infograph.venngage.com/register");
+	}
+
+	public void sendKeys() {
+		driver.findElement(By.xpath("//input[@id='user_first_name']")).sendKeys("QA");
+		driver.findElement(By.xpath("//input[@id='user_last_name']")).sendKeys("Test");
+		driver.findElement(By.xpath("//input[@id='user_email']")).sendKeys("qa.technicaltest@venngage.com");
+		driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("123456");
+
+	}
+
+	public void clickByXpath() {
+
+		driver.findElement(By.xpath("//button[@id='btn_register']")).click();
+
+	}
+
 }
