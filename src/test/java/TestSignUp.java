e0ce9026e98a5d45836f645e14e13fd81fc8e8da
Anchal708 <62824633+Anchal708@users.noreply.github.com>
2020-08-01 22:01:45 -0400

    Add files via upload

--- src/test/java/TestSignUp.java
+++ src/test/java/TestSignUp.java
@@ -1,17 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
 public class TestSignUp {
+	
+
 	
 	private Account account;
 	private SignUpPage signUpPage;
+	Selenium selenium;
 	
-	@Before
+	@BeforeMethod
 	public void setUp(){
-		// Instantiate page objects here
+		
+		selenium.goToUrl();		
+	}
+	
+	@Test (dataProvider = "sign")
+	public void signUpTestWithMissingInputs(String Firstname, String Lastname, String Email, String Password) {
 		
 	}
 	
-	@Test
-	public void testSignUp(){
-		// Write test steps here
-		
+	
+	@DataProvider
+	 public Object[][] sign() {
+	    return new Object[][] {
+	    
+	    	new Object[] {  "QA","Test", "qa.technicaltest@venngage.com", "123456", "" },
+	    	new Object[] { "Test", "qa.technicaltest@venngage.com", "123456", "" },
+	      new Object[] { "QA", "qa.technicaltest@venngage.com", "123456", "" },
+	      new Object[] { "", "", "", "" },
+	     	      
+	    };
 	}
-}
+	}
